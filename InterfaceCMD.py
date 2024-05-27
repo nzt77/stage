d7fd984e50812fc32cbfbf67664f7a067632deeb
nzt77 <noa.pro062@gmail.com>
2024-05-27 16:10:57 +0200

    ajout fonction pour enregistrer une personne dans log.txt

--- InterfaceCMD.py
+++ InterfaceCMD.py
@@ -1,4 +1,5 @@
 from Function import Global
+import datetime
 
 def afficher_menu():
     print("\nInterface CMD Basique")
@@ -7,13 +8,14 @@ def afficher_menu():
     print("3. Soustraction")
     print("4. Multiplication")
     print("5. Division")
-    print("6. Quitter")
+    print("6. Enregistrer un passage")
+    print("7. Quitter")
 
-def obtenir_entree_utilisateur():
+def Input():
     choix = input("Choisir une option: ")
     return choix
 
-def executer_choix(choix):
+def Choix(choix):
     if choix == '1':
         texte = input("Entrez le texte à afficher : ")
         Global.Afficher(texte)
@@ -23,7 +25,7 @@ def executer_choix(choix):
         print(f"Résultat : {Global.Ajouter(a, b)}")
     elif choix == '3':
         a = float(input("Entrez le premier nombre : "))
-        b = float(input("Entrez le secondd nombre : "))
+        b = float(input("Entrez le second nombre : "))
         print(f"Résultat : {Global.Soustraire(a, b)}")
     elif choix == '4':
         a = float(input("Entrez le premier nombre : "))
@@ -33,11 +35,17 @@ def executer_choix(choix):
         a = float(input("Entrez le premier nombre : "))
         b = float(input("Entrez le second nombre : "))
         print(f"Résultat : {Global.Diviser(a, b)}")
-    elif choix == '6':
+    elif choix == '7':
         print("Fermeture")
         return False
+    elif choix == '6':
+        nom = input("Entrez votre nom : ")
+        maintenant = datetime.datetime.now()
+        with open("log.txt", "a") as log_file:
+            log_file.write(f"{nom} - {maintenant.strftime('%Y-%m-%d %H:%M:%S')}\n")
+        print(" Passage noté dans log.txt")
     else:
-        print("Choix non valide. Réessayez : )
+        print("Choix non valide. Réessayez : ")
     return True
 
 def main():
