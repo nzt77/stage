4bc2ab8e3275187fee4fe543126fa2fdbf5fd3dd
nzt77 <noa.pro062@gmail.com>
2024-05-27 12:45:14 +0200

    ajout de fonctions / pouvoir choisir sur l'itnerface

--- Function.py
+++ Function.py
@@ -1,6 +1,24 @@
-# contient les fonctions utilisé par le programme d'interface
-
 class Global:
 
-  def Afficher(Texte):
-    print(Texte)
+    @staticmethod
+    def Afficher(Texte):
+        print(Texte)
+
+    @staticmethod
+    def Ajouter(a, b):
+        return a + b
+
+    @staticmethod
+    def Soustraire(a, b):
+        return a - b
+
+    @staticmethod
+    def Multiplier(a, b):
+        return a * b
+
+    @staticmethod
+    def Diviser(a, b):
+        if b != 0:
+            return a / b
+        else:
+            return "ne peut pas diviser par 0"
--- InterfaceCMD.py
+++ InterfaceCMD.py
@@ -5,3 +5,22 @@ print('interface cmd basique pour naviguer et utiliser des fonctions')
 Global.Afficher('salut')
 
 Global.Afficher('test 2 car lucas est bronze')
+
+
+def executer_choix(choix):
+    if choix == '1':
+
+    elif choix == '2':
+
+    elif choix == '3':
+
+    elif choix == '4':
+
+    elif choix == '5':
+
+    elif choix == '6':
+
+        return False
+    else:
+
+    return True
