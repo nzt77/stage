e6913fe681e99686c13af441626c1bea2a3eb5fd
nzt77 <noa.pro062@gmail.com>
2024-05-28 11:14:02 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,1 +1,10 @@
 #interface pyqt5
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+app = QApplication([])
+window = QMainWindow()
+window.setGeometry(100, 100, 600, 400) 
+window.setWindowTitle('Interface Vide PyQt6')
+window.show()
+app.exec()
