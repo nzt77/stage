5b9fb1c31a965a3feb2dc6a63772df77db078916
nzt77 <noa.pro062@gmail.com>
2024-05-29 15:24:28 +0200

    mise a jour interface reception, animation bouton, mise en page

--- Interface.py
+++ Interface.py
@@ -1,11 +1,36 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget, QGraphicsOpacityEffect, QLabel, QHBoxLayout
 from PyQt6.QtGui import QFont
+from PyQt6.QtCore import QPropertyAnimation, Qt
 from datetime import datetime
+
+class HoverButton(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.effect = QGraphicsOpacityEffect(self)
+        self.setGraphicsEffect(self.effect)
+        self.animation = QPropertyAnimation(self.effect, b"opacity")
+
+
+
+    def enterEvent(self, event):
+        self.animation.stop()
+        self.animation.setDuration(500)
+        self.animation.setStartValue(1)
+        self.animation.setEndValue(0.5)
+        self.animation.start()
+
+    def leaveEvent(self, event):
+        self.animation.stop()
+        self.animation.setDuration(500)
+        self.animation.setStartValue(0.5)
+        self.animation.setEndValue(1)
+        self.animation.start()
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 450, 300)
+        self.setFixedSize(300, 150)
+        self.setWindowOpacity(0.9)
         self.setWindowTitle('Reception')
 
         self.setStyleSheet("background-color: #2c3e50;")  
@@ -14,23 +39,36 @@ class MainWindow(QMainWindow):
         self.layout = QVBoxLayout()
         self.widget.setLayout(self.layout)
 
+        self.label = QLabel("Veuillez entrer votre nom :")
+        self.label.setStyleSheet("color: white; font-size: 16px;")
+        self.layout.addWidget(self.label)
+        self.label.move(50, 0)
+
+
         self.textbox = QLineEdit()
         self.textbox.setStyleSheet("background-color: white; border-radius: 10px; padding: 8px;") 
         self.layout.addWidget(self.textbox)
 
-        self.enter_button = QPushButton("Entrer")
+        button_layout = QHBoxLayout()
+
+        self.enter_button = HoverButton("Entrer")
         self.enter_button.setFixedWidth(100) 
         self.enter_button.setFixedHeight(50)
         self.enter_button.clicked.connect(self.enter)
         self.enter_button.setStyleSheet("QPushButton {background-color: #27ae60; border-radius: 15px; color: white; font-weight: bold;} QPushButton:hover {background-color: #2ecc71;}")  # Style pour le bouton Entrer
         self.layout.addWidget(self.enter_button)
 
-        self.exit_button = QPushButton("Sortir")
+        self.exit_button = HoverButton("Sortir")
         self.exit_button.setFixedWidth(100)  
         self.exit_button.setFixedHeight(50)  
         self.exit_button.clicked.connect(self.exit)
         self.exit_button.setStyleSheet("QPushButton {background-color: #c0392b; border-radius: 15px; color: white; font-weight: bold;} QPushButton:hover {background-color: #e74c3c;}")  # Style pour le bouton Sortir
         self.layout.addWidget(self.exit_button)
+
+        button_layout.addWidget(self.enter_button)
+        button_layout.addWidget(self.exit_button)
+        self.layout.addLayout(button_layout)
+
 
         self.setCentralWidget(self.widget)
 
@@ -39,6 +77,7 @@ class MainWindow(QMainWindow):
 
     def exit(self):
         self.log_action("sorti")
+
 
     def log_action(self, action):
         name = self.textbox.text()
