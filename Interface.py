6d89ac4d33bb0297cdf7d6b81121429e20e99b1a
nzt77 <noa.pro062@gmail.com>
2024-05-29 14:35:22 +0200

    mise a jour interface

--- Interface.py
+++ Interface.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget
+from PyQt6.QtGui import QFont
 from datetime import datetime
 
 class MainWindow(QMainWindow):
@@ -7,23 +8,28 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 450, 300)
         self.setWindowTitle('Reception')
 
+        self.setStyleSheet("background-color: #2c3e50;")  
+
         self.widget = QWidget()
         self.layout = QVBoxLayout()
         self.widget.setLayout(self.layout)
 
         self.textbox = QLineEdit()
+        self.textbox.setStyleSheet("background-color: white; border-radius: 10px; padding: 8px;") 
         self.layout.addWidget(self.textbox)
 
         self.enter_button = QPushButton("Entrer")
-        self.enter_button.setFixedWidth(100)  
-        self.enter_button.setFixedHeight(50)  
+        self.enter_button.setFixedWidth(100) 
+        self.enter_button.setFixedHeight(50)
         self.enter_button.clicked.connect(self.enter)
+        self.enter_button.setStyleSheet("QPushButton {background-color: #27ae60; border-radius: 15px; color: white; font-weight: bold;} QPushButton:hover {background-color: #2ecc71;}")  # Style pour le bouton Entrer
         self.layout.addWidget(self.enter_button)
 
         self.exit_button = QPushButton("Sortir")
         self.exit_button.setFixedWidth(100)  
         self.exit_button.setFixedHeight(50)  
         self.exit_button.clicked.connect(self.exit)
+        self.exit_button.setStyleSheet("QPushButton {background-color: #c0392b; border-radius: 15px; color: white; font-weight: bold;} QPushButton:hover {background-color: #e74c3c;}")  # Style pour le bouton Sortir
         self.layout.addWidget(self.exit_button)
 
         self.setCentralWidget(self.widget)
