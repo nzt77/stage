ee4ddfd1092428ccf96779679b2ed52664372df6
Ikkazz <bosiolucas845@gmail.com>
2024-06-26 22:13:29 +0200

    Changement programme (pas encore complet)

--- DB_user_transaction/testconnexion.py
+++ DB_user_transaction/testconnexion.py
@@ -1,5 +1,12 @@
 import PySimpleGUIWeb as sg
 import yaml
+from flask import Flask, render_template_string, jsonify, request
+import threading
+import webbrowser
+import time
+import requests
+
+app = Flask(__name__)
 
 with open('users.yaml', 'r') as file:
     users_data = yaml.safe_load(file)
@@ -15,13 +22,37 @@ layout = [
     [sg.Button('Login'), sg.Button('Register'), sg.Button('Cancel')]
 ]
 
-window = sg.Window('Page de Connexion', layout)
+window = sg.Window('Page de Connexion', layout, web_port=5000, web_start_browser=False)
 
-while True:
-    event, values = window.read()
-    
-    if event in (sg.WIN_CLOSED, 'Cancel'):
-        break
+def run_flask():
+    app.run(debug=True, use_reloader=False, port=5000)
+
+@app.route('/')
+def index():
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Connexion</title>
+        <script src="https://cdn.jsdelivr.net/npm/@webcomponents/webcomponentsjs@2/webcomponents-bundle.js"></script>
+        <script src="https://unpkg.com/@pysimplegui/pysimpleguiweb"></script>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script>
+            PySimpleGUIWeb.port = 5000;
+            PySimpleGUIWeb.loadWindow('Page de Connexion', '#app');
+        </script>
+    </body>
+    </html>
+    '''
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    event = data.get('event')
+    values = data.get('values')
     
     if event == 'Login':
         username = values['username']
@@ -31,26 +62,10 @@ while True:
         for user in users_data['users']:
             if user['username'] == username and user['password'] == password:
                 valid_user = True
-                sg.popup('Connexion réussie', f'Bienvenue, {username}!')
-                break
+                return jsonify({'message': 'Connexion réussie', 'username': username})
         
-        if not valid_user:
-            sg.popup('Erreur de connexion', 'Nom d\'utilisateur ou mot de passe incorrect')
-        else:
-            permission_layout = [
-                [sg.Text(f'Bienvenue, {username}', size=(15, 1), font=('Helvetica', 18), justification='center')],
-                [sg.Text('Vous avez les permissions suivantes :')],
-                [sg.Text('- Voir vos réservations')],
-                [sg.Button('Logout')]
-            ]
-            
-            permission_window = sg.Window('Permissions', permission_layout)
-            while True:
-                perm_event, perm_values = permission_window.read()
-                if perm_event in (sg.WIN_CLOSED, 'Logout'):
-                    permission_window.close()
-                    break
-
+        return jsonify({'message': 'Nom d\'utilisateur ou mot de passe incorrect'})
+    
     elif event == 'Register':
         username = values['username']
         password = values['password']
@@ -62,11 +77,29 @@ while True:
                 break
         
         if user_exists:
-            sg.popup('Erreur de registre', 'Nom d\'utilisateur déjà utilisé')
+            return jsonify({'message': 'Nom d\'utilisateur déjà utilisé'})
         else:
             new_user = {"username": username, "password": password, "role": "client"}
             users_data['users'].append(new_user)
             save_users(users_data)
-            sg.popup('Registre réussi', 'Utilisateur créé avec succès ! Vous pouvez maintenant vous connecter.')
+            return jsonify({'message': 'Utilisateur créé avec succès ! Vous pouvez maintenant vous connecter.'})
+    
+    return jsonify({'message': 'Invalid event'})
+
+threading.Thread(target=run_flask).start()
+
+time.sleep(2)
+webbrowser.open("http://localhost:5000")
+
+while True:
+    event, values = window.read()
+    
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+    
+    if event in ('Login', 'Register'):
+        response = requests.post('http://localhost:5000/submit', json={'event': event, 'values': values})
+        response_data = response.json()
+        sg.popup(response_data['message'])
 
 window.close()
